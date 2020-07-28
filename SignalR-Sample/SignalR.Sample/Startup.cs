ad52b23bbd0696f5d4121ebc039c62d37f0a74d6
Mike Squibb <msquibb@genesishcs.org>
2020-07-28 09:37:41 -0400

    Add project files.

---
+++ SignalR-Sample/SignalR.Sample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public static class Startup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+    }
+}
---
+++ SignalR-Sample/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR_Sample.Startup))]
+
+namespace SignalR_Sample
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+		}
+	}
+}
